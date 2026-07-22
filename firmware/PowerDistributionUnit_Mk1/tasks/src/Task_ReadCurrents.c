db8fba3f09137cabd67d503a953e28dfd90cd69a
Ravi Shah <ravidhruvishshah@gmail.com>
2026-07-21 23:51:32 -0500

    better readings

--- firmware/PowerDistributionUnit_Mk1/tasks/src/Task_ReadCurrents.c
+++ firmware/PowerDistributionUnit_Mk1/tasks/src/Task_ReadCurrents.c
@@ -4,11 +4,10 @@
 
 static uint16_t current_read_failures = 0;
 
-void Task_ReadCurrents(void *argument)
-{
-    for(;;)
+void Task_ReadCurrents(void* argument) {
+    for (;;)
     {
-        if(PDU_Mk1_CurrentSensing_ReadCurrents() != true)
+        if (PDU_Mk1_CurrentSensing_ReadCurrents() != true)
         {
             current_read_failures++;
             printf("FAIL:READ_CURRENTS_%d\n", current_read_failures);
@@ -17,41 +16,14 @@ void Task_ReadCurrents(void *argument)
 #if (PDU_MK1_PRINT_STATUS_VIA_UART)
         float* currents = PDU_Mk1_CurrentSensing_GetCurrentsPtr();
 
-        printf("\nCurrents\n----------\n"
-                "CH0: %.4f A\n"
-                "CH1: %.4f A\n"
-                "CH2: %.4f A\n"
-                "CH3: %.4f A\n"
-                "CH4: %.4f A\n"
-                "CH5: %.4f A\n"
-                "CH6: %.4f A\n"
-                "CH7: %.4f A\n" 
-                "CH8: %.4f A\n"
-                "CH9: %.4f A\n"
-                "CH10: %.4f A\n"
-                "CH11: %.4f A\n"
-                "CH12: %.4f A\n"
-                "CH13: %.4f A\n"
-                "CH14: %.4f A\n"
-                "CH15: %.4f A\n", 
-                currents[0], 
-                currents[1], 
-                currents[2], 
-                currents[3], 
-                currents[4], 
-                currents[5], 
-                currents[6], 
-                currents[7], 
-                currents[8], 
-                currents[9], 
-                currents[10], 
-                currents[11], 
-                currents[12], 
-                currents[13], 
-                currents[14],
-                currents[15]);
+        printf("\r\n--------- Current Measurements ---------\r\n");
+        printf("----------------------------------------\r\n");
+
+        for (uint8_t i = 0; i < 16; i++) {
+            printf("CH%u: %.4f A\r\n", i, currents[i]);
+        }
 #endif
-        
+
         vTaskDelay(pdMS_TO_TICKS(TASK_READCURRENTS_INTERVAL_MS));
     }
 }
